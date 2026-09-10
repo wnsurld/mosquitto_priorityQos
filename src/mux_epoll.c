7f571639561d4560f2351d8d0216e0ecaaae0298
Lee JunHyuk <wnsgur00199@naver.com>
2026-09-10 16:45:44 +0900

    입력부 우선순위 분류 및 처리 로직 추가

--- src/mux_epoll.c
+++ src/mux_epoll.c
@@ -43,6 +43,17 @@ static void loop_handle_reads_writes(struct mosquitto *context, uint32_t events)
 static struct epoll_event ep_events[MAX_EVENTS];
 
 
+/*우선순위 enum*/
+enum packet_prio {
+	PRIO_QOS0,
+	PRIO_QOS1,
+	PRIO_QOS2,
+	PRIO_NORMAL,
+	PRIO_UNKNOWN
+};
+
+static enum packet_prio classify_topic_from_peek(struct mosquitto *context);
+
 int mux_epoll__init(void)
 {
 	memset(&ep_events, 0, sizeof(struct epoll_event)*MAX_EVENTS);
@@ -152,21 +163,26 @@ int mux_epoll__delete(struct mosquitto *context)
 	return 0;
 }
 
-
 int mux_epoll__handle(void)
-{
+{	
 	struct epoll_event ev;
 	struct mosquitto *context;
 	struct mosquitto__listener_sock *listensock;
 	int event_count;
+	uint64_t qos0_mask = 0;
+	uint64_t qos1_mask = 0;
+	uint64_t qos2_mask = 0;
+	uint64_t normal_mask = 0;
+	uint64_t unknown_mask = 0;
+	int timeout_ms = db.next_event_ms;
+	
 
 	memset(&ev, 0, sizeof(struct epoll_event));
 #if defined(WITH_WEBSOCKETS)
 	event_count = epoll_wait(db.epollfd, ep_events, MAX_EVENTS, 100);
 #else
-	event_count = epoll_wait(db.epollfd, ep_events, MAX_EVENTS, db.next_event_ms);
+	event_count = epoll_wait(db.epollfd, ep_events, MAX_EVENTS, timeout_ms);
 #endif
-
 	db.now_s = mosquitto_time();
 	db.now_real_s = time(NULL);
 
@@ -178,11 +194,54 @@ int mux_epoll__handle(void)
 			break;
 		case 0:
 			break;
+		case 1:
+			/*이벤트 하나일 때*/
+			context = ep_events[0].data.ptr;
+			if(context->ident == id_client){
+				loop_handle_reads_writes(context, ep_events[0].events);
+			}else if(context->ident == id_listener){
+				listensock = ep_events[0].data.ptr;
+				if(ep_events[0].events & (EPOLLIN | EPOLLPRI)){
+					while((context = net__socket_accept(listensock)) != NULL){
+					}
+				}
+#if defined(WITH_WEBSOCKETS) && WITH_WEBSOCKETS == WS_IS_LWS
+			}else if(context->ident == id_listener_ws){
+				/* Nothing needs to happen here, because we always call lws_service in the loop.
+				 * The important point is we've been woken up for this listener. */
+#endif
+			}
+			break;
 		default:
+			/*이벤트 2개 이상*/
 			for(int i=0; i<event_count; i++){
 				context = ep_events[i].data.ptr;
 				if(context->ident == id_client){
-					loop_handle_reads_writes(context, ep_events[i].events);
+
+					if(!(ep_events[i].events & EPOLLIN)){
+						continue;
+					}
+
+					switch(classify_topic_from_peek(context)){
+						case PRIO_QOS0:
+							qos0_mask |= (1ULL << i);
+							break;
+	
+						case PRIO_QOS1:
+							qos1_mask |= (1ULL << i);
+							break;
+
+						case PRIO_QOS2:
+							qos2_mask |= (1ULL << i);
+							break;
+						case PRIO_UNKNOWN:
+							unknown_mask |= (1ULL << i);
+							break;	
+						case PRIO_NORMAL:
+						default:
+							normal_mask |= (1ULL << i);
+							break;
+					}
 				}else if(context->ident == id_listener){
 					listensock = ep_events[i].data.ptr;
 
@@ -197,7 +256,47 @@ int mux_epoll__handle(void)
 #endif
 				}
 			}
+
+			/*인덱스 추출*/
+			while(1){
+				int idx;
+				
+				if(qos0_mask != 0){
+					idx = __builtin_ctzll(qos1_mask);
+					context = ep_events[idx].data.ptr;
+					loop_handle_reads_writes(context, ep_events[idx].events);
+					qos0_mask &= ~(1ULL << idx);
+
+				}else if(qos1_mask != 0){
+					idx = __builtin_ctzll(mid_mask);
+					context = ep_events[idx].data.ptr;
+					loop_handle_reads_writes(context, ep_events[idx].events);
+					qos1_mask &= ~(1ULL << idx);
+
+				}else if(qos2_mask != 0){
+					idx = __builtin_ctzll(low_mask);
+					context = ep_events[idx].data.ptr;
+					loop_handle_reads_writes(context, ep_events[idx].events);
+					qos2_mask &= ~(1ULL << idx);
+
+				}else if(normal_mask != 0){
+					idx = __builtin_ctzll(normal_mask);
+					context = ep_events[idx].data.ptr;
+					loop_handle_reads_writes(context, ep_events[idx].events);
+					normal_mask &= ~(1ULL << idx);
+
+				}else if(unknown_mask != 0){
+					idx = __builtin_ctzll(unknown_mask);
+					context = ep_events[idx].data.ptr;
+					loop_handle_reads_writes(context, ep_events[idx].events);
+					unknown_mask &= ~(1ULL << idx);
+				}
+				else{
+					break;
+				}
+			}
 	}
+
 	return MOSQ_ERR_SUCCESS;
 }
 
@@ -209,6 +308,89 @@ int mux_epoll__cleanup(void)
 	return MOSQ_ERR_SUCCESS;
 }
 
+/*토픽을 통한 우선순위 분류*/
+static enum packet_prio classify_topic_from_peek(struct mosquitto *context)
+{
+	unsigned char buf[256];
+	ssize_t n;
+	size_t pos;
+	size_t rem_len_bytes;
+	uint32_t remaining_length;
+	uint32_t multiplier;
+	uint16_t topic_len;
+	const unsigned char *topic_ptr;
+
+	if(!context || context->sock == INVALID_SOCKET){
+		return PRIO_NORMAL;
+	}
+
+	n = recv(context->sock, buf, sizeof(buf), MSG_PEEK);
+	if(n <= 0){
+		return PRIO_UNKNOWN;
+	}
+
+	/* 최소 fixed header 2바이트는 있어야 함 */
+	if(n < 2){
+		return PRIO_UNKNOWN;
+	}
+
+	/* MQTT Control Packet Type = upper nibble
+	 * PUBLISH = 0x3
+	 */
+	if(((buf[0] >> 4) & 0x0F) != 0x03){
+		return PRIO_NORMAL;
+	}
+
+	/* Remaining Length 파싱 */
+	pos = 1;
+	remaining_length = 0;
+	multiplier = 1;
+	rem_len_bytes = 0;
+
+	do{
+		if(pos >= (size_t)n){
+			return PRIO_UNKNOWN;
+		}
+
+		remaining_length += (buf[pos] & 0x7F) * multiplier;
+		multiplier *= 128;
+		rem_len_bytes++;
+	}while((buf[pos++] & 0x80) != 0 && rem_len_bytes < 4);
+
+	if(rem_len_bytes == 4 && (buf[pos-1] & 0x80) != 0){
+		return PRIO_UNKNOWN;
+	}
+
+	/* topic length 2바이트 확인 */
+	if(pos + 2 > (size_t)n){
+		return PRIO_UNKNOWN;
+	}
+
+	topic_len = (uint16_t)((buf[pos] << 8) | buf[pos+1]);
+	pos += 2;
+
+	if(topic_len < 7){
+		return PRIO_NORMAL;
+	}
+
+	/* topic 문자열 전체가 peek buffer 안에 있는지 확인 */
+	if(pos + topic_len > (size_t)n){
+		return PRIO_UNKNOWN;
+	}
+
+
+	topic_ptr = &buf[pos];
+
+	if(memcmp(topic_ptr + topic_len - 6, "/pQoS0", 6) == 0){
+		return PRIO_QOS0;
+	}else if(memcmp(topic_ptr + topic_len - 6, "/pQoS1", 6) == 0){
+		return PRIO_QOS1;
+	}else if(memcmp(topic_ptr + topic_len - 6, "/pQoS2", 6) == 0){
+		return PRIO_QOS2;
+	}else{
+		return PRIO_NORMAL;
+	}
+}
 
 static void loop_handle_reads_writes(struct mosquitto *context, uint32_t events)
 {
